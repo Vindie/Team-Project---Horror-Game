acff23bd25fbbd2ab66e5e496b99d32bb71e22fb
Fornan-II <Fornan2@gmail.com>
2018-03-29 16:31:20 -0400

    FPS_Controller Started
    Include FPS_Controller and FPS_InputPoller

---
+++ Assets/Max_Scripts/FPS_Controller.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_Controller : PlayerController {
+
+	// Use this for initialization
+	protected override void Start () {
+        base.Start();
+        LogInputStateInfo = false;
+        LogHUDUpdateError = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Unused
+	}
+
+    public override void DefaultBinds()
+    {
+        AddAxis("LookHorizontal", LookHorizontal);
+        AddAxis("LookVertical", LookVertical);
+        AddAxis("MoveHorizontal", Horizontal);
+        AddAxis("MoveVertical", Vertical);
+        AddButton("Fire1", Fire1);
+        AddButton("Fire2", Fire2);
+        AddButton("Fire3", Fire3);
+    }
+
+    public virtual void LookHorizontal(float value)
+    {
+        
+    }
+
+    public virtual void LookVertical(float value)
+    {
+
+    }
+
+    public override void Horizontal(float value)
+    {
+        
+    }
+
+    public override void Vertical(float value)
+    {
+        
+    }
+
+    public override void Fire1(bool value)
+    {
+        
+    }
+
+    public override void Fire2(bool value)
+    {
+
+    }
+
+    public override void Fire3(bool value)
+    {
+        
+    }
+}
---
+++ Assets/Max_Scripts/FPS_InputPoller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS_InputPoller : InputPoller {
+
+    public override InputState GetPlayer1Input()
+    {
+        // Example Input binding. 
+        InputState IS = InputState.GetBlankState();
+        IS.AddAxis("LookHorizontal", Input.GetAxis("Horizontal"));  //Mouse?
+        IS.AddAxis("LookVertical", Input.GetAxis("Vertical"));      //Mouse?
+        IS.AddAxis("MoveHorizontal", Input.GetAxis("Horizontal"));
+        IS.AddAxis("MoveVertical", Input.GetAxis("Vertical"));
+        IS.AddButton("Fire1", Input.GetButton("Fire1"));
+        IS.AddButton("Fire2", Input.GetButton("Fire2"));
+        IS.AddButton("Fire3", Input.GetButton("Fire3"));
+        return IS;
+    }
+}
