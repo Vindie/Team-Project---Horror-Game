8c1b1f65d3408b817eed03ca1bfe6e318db5778f
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-04-16 15:21:22 -0400

    New Plan for Door
    Exit door only, doesn't actually open.

---
+++ Assets/Max_Scripts/ExitDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : Interactable {
+
+    public bool isLocked = true;
+
+    public virtual void SetDoorLock(bool value)
+    {
+        isLocked = value;
+    }
+
+    protected override bool ProcessInteraction(Actor source, Controller instigator)
+    {
+        if(isLocked)
+        {
+            return false;
+        }
+        //PlayerController
+        //Fade to black
+        //Fade in splash text - "Escaped the mine" or something
+        //Return to main menu
+
+        return true;
+    }
+}
