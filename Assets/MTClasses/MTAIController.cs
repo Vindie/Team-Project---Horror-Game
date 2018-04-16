74ca89a70396b9ecef9c79f05dc103411b1a0563
mtruesdale382 <37198866+mtruesdale382@users.noreply.github.com>
2018-04-16 15:56:17 -0400

    Torches Update
    AI can put out torches as it passes

--- Assets/MTClasses/MTAIController.cs
+++ Assets/MTClasses/MTAIController.cs
@@ -9,42 +9,40 @@ public class MTAIController : MonoBehaviour
     public GameObject playerPawn;
     public int fieldOfViewDegrees = 180;
     public int damageFactor = 10;
+    public int locationIndex = 0;
     public float moveSpeed = 20.0f;
     public float armsReach = 10.0f;
+    public int sightRadius = 10;
     Vector3 locationLastPlayerSeen;
+    Vector3[] locations;
     Vector3 torchLocation;
+    Collider[] torches;
 
 
     // Use this for initialization
     void Start()
-    {   
+    {
         locationLastPlayerSeen = playerPawn.transform.position;
     }
 
     public void Update()
     {
-        CanSeePlayer("Player");
+        CanSeePlayer("Player"); //if cant see player move randomly
     }
 
     public void FixedUpdate()
     {
-        if (CanSeeTorch("Torch"))
+        putTorchesOut();
+
+        if(!CanSeePlayer("Player"))
         {
-            moveTowards(torchLocation, moveSpeed);
+            moveTowards(locations[locationIndex], moveSpeed);
         }
-        
-        if(!CanSeeTorch("Torch"))
-        {
-            moveTowards(locationLastPlayerSeen, moveSpeed);
-        }     
+
+        moveTowards(locationLastPlayerSeen, moveSpeed);
+
         checkDamageDistance();
     }
-    /*
-     * puts out torches in passing
-     * if player has torch priority to snuff out torch
-     * dim light with lighter
-     * check player's vision to detemine if monster can snuff out light(dot product)
-     * */
 
     public bool CanSeePlayer(string tag)
     {
@@ -68,28 +66,6 @@ public class MTAIController : MonoBehaviour
         return false;
     }
 
-    public bool CanSeeTorch(string tag)
-    {
-        RaycastHit hit;
-        Vector3 rayDirection = playerPawn.transform.position - transform.position;
-
-        if ((Vector3.Angle(rayDirection, transform.forward)) <= fieldOfViewDegrees * 1f)
-        {
-            // Checks if an object with a given tag is within the given field of view
-            if (Physics.Raycast(transform.position, rayDirection, out hit))
-            {
-                if (hit.transform.CompareTag(tag))
-                {
-                    torchLocation = hit.point;
-                    print("Saw Torch");
-                    return true;
-                }
-                return false;
-            }
-        }
-
-        return false;
-    }
     public void moveTowards(Vector3 LocationToMoveTowards, float moveSpeed)
     {
         transform.position = Vector3.MoveTowards(transform.position, LocationToMoveTowards, moveSpeed * Time.deltaTime);
@@ -100,13 +76,33 @@ public class MTAIController : MonoBehaviour
         Pawn pp = playerPawn.GetComponent<Pawn>();
         float distanceToPlayer = Vector3.Distance(playerPawn.transform.position, gameObject.transform.position);
         print("Distance to player: " + distanceToPlayer);
-        if(pp)
+        if (pp)
         {
             if (distanceToPlayer < armsReach)
             {
-                print("Distance to player: " + distanceToPlayer);
+                //print("Distance to player: " + distanceToPlayer);
                 pp.TakeDamage(gameObject.GetComponent<Actor>(), damageFactor);
             }
+        }
+    }
+
+    public void putTorchesOut()
+    {
+        int index = 0;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, sightRadius);
+        while (index < hitColliders.Length)
+        {
+            if (hitColliders[index].gameObject.tag == "Torch")
+            {
+                Torch ts = hitColliders[index].gameObject.GetComponent<Torch>();
+                if (ts)
+                {
+                    print("Got Light");
+                    ts.LightOff();                    
+                }        
+            }
+
+            index++;
         }
     }
 }
---
+++ Assets/MTClasses/Torch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    public GameObject firePrefab;
+	void Start ()
+    {
+        LightOn(); 
+    }
+
+    public void LightOn()
+    {
+        firePrefab.SetActive(true);
+    }
+
+    public void LightOff()
+    {
+        firePrefab.SetActive(false); 
+    }
+
+}
