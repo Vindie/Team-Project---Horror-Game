0dfc1bbe781d2f19fbfa5a8b0134f4c30f102191
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-04-19 13:34:13 -0400

    Key Stuff
    Key implemented - temp model

---
+++ Assets/Max_Scripts/Key_Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key_Item : Item {
+
+    public override bool Use(Actor user)
+    {
+        FPS_Pawn FPP = (FPS_Pawn)user;
+        if(!FPP) { return false; }
+
+        GameObject objectKeyIsBeingUsedOn = FPP.GetInteractableObject();
+        if(!objectKeyIsBeingUsedOn) { return false; }
+
+        ExitDoor door = objectKeyIsBeingUsedOn.GetComponent<ExitDoor>();
+        if(!door) { return false; }
+
+        door.isLocked = false;
+        //Delete this item, or remove this component from the object.
+        return true;
+    }
+}
