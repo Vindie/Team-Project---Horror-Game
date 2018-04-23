95b1e497f1d4803158f8682c0c325150b03e85cd
Fornan-II <fornan2@gmail.com>
2018-04-22 22:16:07 -0400

    It Follows
    The eye always watches the player.

---
+++ Assets/Max_Scripts/AlwaysFaceCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysFaceCamera : MonoBehaviour {
+
+    public Transform CameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		if(!CameraTransform)
+        {
+            GetCameraTransform();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(CameraTransform)
+        {
+            transform.forward = transform.position - CameraTransform.position;
+        }
+        else
+        {
+            GetCameraTransform();
+        }
+	}
+
+    protected void GetCameraTransform()
+    {
+        Camera cam = FindObjectOfType<Camera>();
+        if(!cam) { return; }
+        CameraTransform = cam.transform;
+    }
+}
