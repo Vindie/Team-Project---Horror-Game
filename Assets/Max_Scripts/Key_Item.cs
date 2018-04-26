38e9f641dba47c7fe26ab5556d077de4029414de
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2018-04-26 13:15:47 -0400

    Don't break everything
    Please don't break everything

--- Assets/Max_Scripts/Key_Item.cs
+++ Assets/Max_Scripts/Key_Item.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Key_Item : Interactable {
 
+    public float hintPopupTime = 5.0f;
+    public string popupHint = "Picked up mine key";
+
+    protected float hintPopupTimeRemaining = 0.0f;
+
     protected override bool ProcessInteraction(Actor source, Controller instigator)
     {
         FPS_Pawn FPP = (FPS_Pawn)source;
@@ -12,6 +17,22 @@ public class Key_Item : Interactable {
         FPP.hasKey = true;
         Destroy(gameObject);
         return true;
+    }
+
+    protected virtual IEnumerator ShowPopupHint()
+    {
+        MenuScript ms = FindObjectOfType<MenuScript>();
+        if (ms)
+        {
+            ms.SetGameSmallText(true, popupHint);
+            hintPopupTimeRemaining = hintPopupTime;
+            while (hintPopupTimeRemaining > 0)
+            {
+                hintPopupTimeRemaining -= Time.deltaTime;
+                yield return null;
+            }
+            ms.SetGameSmallText(false);
+        }
     }
 
     /*protected Light _keyLight;
