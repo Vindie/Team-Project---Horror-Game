2d06f35ebc1898c210ca0512e57108dde99a830d
Fornan-II <fornan2@gmail.com>
2018-04-30 22:27:59 -0400

    Random Key Spawning Code
    Not yet tested.

--- Assets/Max_Scripts/HorrorGame.cs
+++ Assets/Max_Scripts/HorrorGame.cs
@@ -6,20 +6,26 @@ public class HorrorGame : Game {
 
     public float GameEndLingerTime = 5.0f;
     public float FadeTime = 10.0f;
-    
-    public string loseText = "Y o u  h a v e  d i e d .";   //A E S T H E T I C
-    public string winText = "Y o u  h a v e  e s c a p e d  t h e  m i n e .";
+
+    public string loseText = "Y  o  u    h  a  v  e    d  i  e  d  .";   //A E S T H E T I C
+    public string winText = "Y  o  u    h  a  v  e    e  s  c  a  p  e  d    t  h  e    m  i  n  e  .";
+
+    public GameObject keyPrefab;
+
     protected MenuScript _ms;
+    protected Key_Spawn[] _keyPotentialSpawns;
 
     public override void Awake()
     {
         base.Awake();
         _ms = FindObjectOfType<MenuScript>();
+        RefreshKey_SpawnList();
+        SpawnTheKeyAt(GetRandomKey_Spawn());
     }
 
     protected virtual void Update()
     {
-        if(!_ms)
+        if (!_ms)
         {
             _ms = FindObjectOfType<MenuScript>();
         }
@@ -29,7 +35,7 @@ public class HorrorGame : Game {
     public virtual bool EndGame(bool victory)
     {
         LOG("Game ending in " + GameEndLingerTime + " seconds.");
-        if(_ms)
+        if (_ms)
         {
             if (victory)
             {
@@ -41,7 +47,7 @@ public class HorrorGame : Game {
             }
 
             ImageEffectManager cam = FindObjectOfType<ImageEffectManager>();
-            if(cam)
+            if (cam)
             {
                 cam.CrossFadeBlack(true, FadeTime);
             }
@@ -54,15 +60,45 @@ public class HorrorGame : Game {
     protected virtual IEnumerator LingerBeforeSceneChange()
     {
         float countDown = GameEndLingerTime;
-        while(countDown > 0)
+        while (countDown > 0)
         {
             countDown -= Time.deltaTime;
             yield return null;
         }
-        
-        if(_ms)
+
+        if (_ms)
         {
             _ms.ReturnToMainMenu();
         }
     }
+
+    #region Key Spawning
+    protected void SpawnTheKeyAt(Transform keySpawnPosition)
+    {
+        if(!keyPrefab)
+        {
+            LOG_ERROR("HorrorGame: Missing Key Prefab");
+            return;
+        }
+
+        GameObject key = Factory(keyPrefab, transform.position, transform.rotation);
+        if(!key) { LOG_ERROR("HorrorGame: Key failed to spawn."); }
+    }
+
+    void RefreshKey_SpawnList()
+    {
+        _keyPotentialSpawns = FindObjectsOfType<Key_Spawn>();
+    }
+
+    Transform GetRandomKey_Spawn()
+    {
+        if (_keyPotentialSpawns == null) { RefreshKey_SpawnList(); }
+
+        int index = (int)(Random.value * _keyPotentialSpawns.Length);
+
+        if (_keyPotentialSpawns.Length == index) { index = 0; }
+        
+        return _keyPotentialSpawns[index].transform;
+    }
+    #endregion
 }
