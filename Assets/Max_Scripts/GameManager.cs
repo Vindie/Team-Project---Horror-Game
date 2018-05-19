6f09bb46d66f4488d78cecdf2f126766a0f872ae
Fornan-II <fornan2@gmail.com>
2018-05-18 20:09:34 -0400

    Settings Saving/Loading Code
    Untested.

---
+++ Assets/Max_Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class GameManager : MonoBehaviour {
+
+    public Settings gameSettings;
+
+	protected void Start () {
+        //There can only be one
+        if (FindObjectOfType<GameManager>())
+        {
+            Destroy(this);
+        }
+        else
+        {
+            DontDestroyOnLoad(this);
+
+            InitializeSettings();
+        }
+	}
+
+    protected void InitializeSettings()
+    {
+        if(!LoadSettings())
+        {
+            gameSettings = new Settings();
+            gameSettings.ResetToDefaultSettings();
+        }
+    }
+
+    public void SaveSettings()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream settingsFile = File.Create("settings.binary");
+
+        formatter.Serialize(settingsFile, gameSettings);
+
+        settingsFile.Close();
+    }
+
+    //Returns true if there is a file to load.
+    public bool LoadSettings()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream settingsFile = File.Open("settings.binary", FileMode.Open);
+        if(settingsFile == null)
+        {
+            return false;
+        }
+
+        gameSettings = (Settings)formatter.Deserialize(settingsFile);
+        settingsFile.Close();
+        return true;
+    }
+}
---
+++ Assets/Max_Scripts/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Settings {
+    public float MouseSensitivity;
+    public float Volume;
+    public float Brightness;
+    
+    public void ResetToDefaultSettings()
+    {
+        MouseSensitivity = 1.0f;
+        Volume = 1.0f;
+        Brightness = 1.0f;
+    }
+}
