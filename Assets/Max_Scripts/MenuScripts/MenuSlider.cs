b6a9048909753e96453c42ff5f56b8c78aeba9a5
Fornan-II <fornan2@gmail.com>
2018-05-21 19:19:45 -0400

    Settings UI Work
    Displays the value of the settings that the player sets.

--- Assets/Max_Scripts/MenuScripts/MenuSlider.cs
+++ Assets/Max_Scripts/MenuScripts/MenuSlider.cs
@@ -7,22 +7,36 @@ public class MenuSlider : MonoBehaviour
 {
     public enum SettingType { MOUSESENSITIVITY, VOLUME, BRIGHTNESS };
     public SettingType ControlledSetting;
+    public Text label;
+    public string labelText = "Undefined";
 
     protected delegate void delegateMethod(float value);
     protected Dictionary<SettingType, delegateMethod> SettingDictionary;
 
     protected GameManager _gm;
+    protected Slider _sl;
 
     // Use this for initialization
     protected virtual void Start()
     {
         InitializeSettingsDictionary();
         InitializeSlider();
+        UpdateLabel();
     }
 
     public virtual void UpdateSetting(float value)
     {
         SettingDictionary[ControlledSetting].Invoke(value);
+        UpdateLabel();
+    }
+
+    protected virtual void UpdateLabel()
+    {
+        if (!label) { return; }
+
+        int percentage = (int)(100.0f * (_sl.value / (_sl.maxValue - _sl.minValue)));
+
+        label.text = labelText + ":" + percentage + "%"; 
     }
 
     protected virtual void InitializeSettingsDictionary()
@@ -39,18 +53,18 @@ public class MenuSlider : MonoBehaviour
     {
         if (!_gm) { _gm = GameObject.FindObjectOfType<GameManager>(); }
         if (!_gm) { return; }
-        Slider sl = gameObject.GetComponent<Slider>();
+        _sl = gameObject.GetComponent<Slider>();
         if(ControlledSetting == SettingType.MOUSESENSITIVITY)
         {
-            sl.value = _gm.gameSettings.MouseSensitivity;
+            _sl.value = _gm.gameSettings.MouseSensitivity;
         }
         else if(ControlledSetting == SettingType.VOLUME)
         {
-            sl.value = _gm.gameSettings.Volume;
+            _sl.value = _gm.gameSettings.Volume;
         }
         else if(ControlledSetting == SettingType.BRIGHTNESS)
         {
-            sl.value = _gm.gameSettings.Brightness;
+            _sl.value = _gm.gameSettings.Brightness;
         }
     }
 
